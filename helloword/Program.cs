00db6497374e6fe65f44e74b7c2db41cf4e48926
jonatan <jonatantirado316539@correo.itm.edu.co>
2024-03-17 15:45:37 -0500

    add new code brithday

--- helloword/Program.cs
+++ helloword/Program.cs
@@ -1,13 +1,29 @@
-﻿using System.Reflection.Metadata.Ecma335;
-
-namespace helloword{ 
+﻿namespace helloword{ 
     class Program{ 
-protected Program() {}
+
     static void Main(){ 
-      
-          var name = "1david";
-         Console.WriteLine($"hello word! {name}");
-         Console.WriteLine();
+          
+           DateOnly dateConverted = new DateOnly();
+           string nameImput;
+           string  birthdayInput;
+          Console.WriteLine("bienvenido al calculador de años");
+          Console.WriteLine("escribe tu nombre: ");
+          nameImput = Console.ReadLine();
+          Console.WriteLine($"un gusto conocerete! {nameImput}");
+          Console.WriteLine("escribe tu fecha de nacimiento en dd/mm/yy: ");
+           birthdayInput= Console.ReadLine();
+           bool isdatevalid=DateOnly.TryParse(birthdayInput,out dateConverted);
+           if(isdatevalid==false) Console.WriteLine($"la fecha es invalida {birthdayInput}");
+           Console.ReadLine();
        }
      }
+    
+
+    public class Person{
+      public string name{get; set;}
+      public int Age{get; set;}
+
+      public DateOnly birthday {get; set;}
+      }
     }
+
