bd0e06b8d9fe0f659d07216f0e101bb03e625508
jonatan <jonatantirado316539@correo.itm.edu.co>
2024-03-17 17:33:32 -0500

    programacion barbero

---
+++ programacion/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        string colorBarberoVerde, colorBarberoRojo, colorBarberoAzul;
+
+        for (int i = 1; i <= 3; i++)
+        {
+    
+            colorBarberoVerde = "";
+            colorBarberoRojo = "";
+            colorBarberoAzul = "";
+
+        
+            if (i == 1)
+            {
+                colorBarberoVerde = "verde";
+            }
+            else if (i == 2)
+            {
+                colorBarberoRojo = "rojo";
+            }
+            else if (i == 3)
+            {
+                colorBarberoAzul = "azul";
+            }
+
+            // Verificamos las condiciones de las afirmaciones
+            if (colorBarberoVerde == "verde" && colorBarberoRojo != "rojo" && colorBarberoAzul != "azul")
+            {
+                Console.WriteLine("El barbero de la Barbería  tiene el cabello azul " + colorBarberoAzul );
+                break;
+            }
+        }
+    }
+}
+
+
